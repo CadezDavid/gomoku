308bc932d1257c459bed54d6141df650f9280c18
David Čadež <david.cadez7@gmail.com>
2021-04-26 11:10:32 +0200

    Zacel delat gui

--- src/gui/Okno.java
+++ src/gui/Okno.java
@@ -5,27 +5,46 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JFrame;
-import javax.swing.event.ChangeEvent;
-import javax.swing.event.ChangeListener;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 
-public class Okno extends JFrame implements ActionListener, ChangeListener {
+public class Okno extends JFrame implements ActionListener {
+
+    private JMenuItem igraClovekClovek;
+    private JMenuItem igraRacunalnikClovek;
 
     public Okno(int sirina, int visina) {
+        // Vse to sem vzel s predavanj ...
         this.setTitle("Gomoku");
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-		this.setLayout(new GridBagLayout());
+        this.setLayout(new GridBagLayout());
 
         // polje
-        polje = new Polje();
+        Platno polje = new Platno();
+        add(polje);
+
+        // menu
+        JMenuBar menu_bar = new JMenuBar();
+        this.setJMenuBar(menu_bar);
+        JMenu igra_menu = new JMenu("Nova igra");
+        menu_bar.add(igra_menu);
+
+        igraClovekClovek = new JMenuItem("Človek – Človek");
+        igra_menu.add(igraClovekClovek);
+        igraClovekClovek.addActionListener(this);
+
+        igraRacunalnikClovek = new JMenuItem("Računalnik – Človek");
+        igra_menu.add(igraRacunalnikClovek);
+        igraRacunalnikClovek.addActionListener(this);
     }
 
-	@Override
-	public void stateChanged(ChangeEvent arg0) {
-		
-	}
-
-	@Override
-	public void actionPerformed(ActionEvent arg0) {
-		
-	}
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == igraRacunalnikClovek) {
+            // ustvari novo igro ...
+        } else if (e.getSource() == igraClovekClovek) {
+            // ustvari novo igro ...
+        }
+    }
 }
---
+++ src/gui/Platno.java
@@ -0,0 +1,42 @@
+package gui;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+import javax.swing.JPanel;
+
+public class Platno extends JPanel implements MouseListener {
+
+    public Platno() {
+        setPreferredSize(new Dimension(200, 200));
+        setBackground(Color.WHITE);
+        // addMouseListener(this);
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent arg0) {
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent arg0) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent arg0) {
+
+    }
+
+    @Override
+    public void mousePressed(MouseEvent arg0) {
+
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent arg0) {
+
+    }
+}
