4ea430543175e849b6698466e81203e3f4d80077
David Čadež <david.cadez7@gmail.com>
2021-04-27 22:55:53 +0200

    Dodal okno in platno

--- src/gui/Okno.java
+++ src/gui/Okno.java
@@ -1,18 +1,32 @@
 package gui;
 
+import java.awt.Font;
+import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.EnumMap;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 
+import logika.Igra;
+import logika.Igralec;
+import logika.Vrsta;
+import logika.Zetoni;
+import vodja.Vodja;
+
 public class Okno extends JFrame implements ActionListener {
+
+    private Platno platno;
 
     private JMenuItem igraClovekClovek;
     private JMenuItem igraRacunalnikClovek;
+
+    private JLabel status;
 
     public Okno(int sirina, int visina) {
         // Vse to sem vzel s predavanj ...
@@ -20,9 +34,9 @@ public class Okno extends JFrame implements ActionListener {
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
         this.setLayout(new GridBagLayout());
 
-        // polje
-        Platno polje = new Platno();
-        add(polje);
+        // platno
+        platno = new Platno();
+        add(platno);
 
         // menu
         JMenuBar menu_bar = new JMenuBar();
@@ -37,14 +51,80 @@ public class Okno extends JFrame implements ActionListener {
         igraRacunalnikClovek = new JMenuItem("Računalnik – Človek");
         igra_menu.add(igraRacunalnikClovek);
         igraRacunalnikClovek.addActionListener(this);
+
+        // statusna vrstica
+        status = new JLabel();
+        status.setFont(new Font("Source Code Pro", Font.BOLD, 18));
+        GridBagConstraints status_layout = new GridBagConstraints();
+        status_layout.gridx = 0;
+        status_layout.gridy = 1;
+        status_layout.anchor = GridBagConstraints.CENTER;
+        getContentPane().add(status, status_layout);
+
+        status.setText("Izberite igro.");
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
+        EnumMap<Zetoni, Igralec> igralca = new EnumMap<Zetoni, Igralec>(Zetoni.class);
         if (e.getSource() == igraRacunalnikClovek) {
-            // ustvari novo igro ...
+            igralca.put(Zetoni.CRNI, new Igralec(Vrsta.RACUNALNIK, "milan"));
+            igralca.put(Zetoni.BELI, new Igralec(Vrsta.CLOVEK, "janko"));
         } else if (e.getSource() == igraClovekClovek) {
-            // ustvari novo igro ...
+            igralca.put(Zetoni.BELI, new Igralec(Vrsta.CLOVEK, "marcel"));
+            igralca.put(Zetoni.CRNI, new Igralec(Vrsta.CLOVEK, "ciril"));
         }
+        Vodja.ustvariNovoIgro(igralca);
     }
+
+    public void osveziGUI() {
+        switch (Vodja.getIgra().getStanje()) {
+            case V_TEKU:
+                String x = Vodja.getIgra().getNapotezi().equals(Zetoni.BELI) ? "beli" : "črni";
+                status.setText("Na potezi je " + x + ".");
+                break;
+            case ZMAGA_BELI:
+                status.setText("Zmagal je beli.");
+                break;
+            case ZMAGA_CRNI:
+                status.setText("Zmagal je črni.");
+                break;
+            case NEODLOCENO:
+                status.setText("Neodločeno.");
+                break;
+        }
+        platno.repaint();
+    }
+
+	public Platno getPlatno() {
+		return platno;
+	}
+
+	public void setPlatno(Platno platno) {
+		this.platno = platno;
+	}
+
+	public JMenuItem getIgraClovekClovek() {
+		return igraClovekClovek;
+	}
+
+	public void setIgraClovekClovek(JMenuItem igraClovekClovek) {
+		this.igraClovekClovek = igraClovekClovek;
+	}
+
+	public JMenuItem getIgraRacunalnikClovek() {
+		return igraRacunalnikClovek;
+	}
+
+	public void setIgraRacunalnikClovek(JMenuItem igraRacunalnikClovek) {
+		this.igraRacunalnikClovek = igraRacunalnikClovek;
+	}
+
+	public JLabel getStatus() {
+		return status;
+	}
+
+	public void setStatus(JLabel status) {
+		this.status = status;
+	}
 }
--- src/gui/Platno.java
+++ src/gui/Platno.java
@@ -1,22 +1,103 @@
 package gui;
 
+import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 
 import javax.swing.JPanel;
 
+import logika.Polje;
+import logika.Vrsta;
+import logika.Zetoni;
+import splosno.Koordinati;
+import vodja.Vodja;
+
 public class Platno extends JPanel implements MouseListener {
 
+    private final static double LINE_WIDTH = 0.05;
+    private final static double PADDING = 0.05;
+
     public Platno() {
-        setPreferredSize(new Dimension(200, 200));
-        setBackground(Color.WHITE);
-        // addMouseListener(this);
+        setPreferredSize(getPreferredSize());
+        setBackground(Color.GREEN);
+        addMouseListener(this);
+    }
+
+    public Dimension getPreferredSize() {
+        return new Dimension(400, 400);
+    }
+
+    private double squareWidth() {
+        return Math.min(getWidth(), getHeight()) / 15;
+    }
+
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        Graphics2D g2 = (Graphics2D) g;
+        double w = squareWidth();
+
+        // narišemo črte
+        g2.setColor(Color.BLACK);
+        g2.setStroke(new BasicStroke((float) (w * LINE_WIDTH)));
+        for (int i = 0; i < 16; i++) {
+            g2.drawLine((int) (i * w), (int) (0), (int) (i * w), (int) (15 * w));
+            g2.drawLine((int) (0), (int) (i * w), (int) (15 * w), (int) (i * w));
+        }
+
+        if (Vodja.getIgra() != null) {
+            Polje[][] plosca = Vodja.getIgra().getPlosca();
+            for (int i = 0; i < 15; i++) {
+                for (int j = 0; j < 15; j++) {
+                    Koordinati k = new Koordinati(i, j);
+                    switch (plosca[i][j]) {
+                        case BELI:
+                            paintSquare(g2, Zetoni.BELI, k);
+                            break;
+                        case CRNI:
+                            paintSquare(g2, Zetoni.CRNI, k);
+                            break;
+                        case PRAZNO:
+                            break;
+                    }
+                }
+            }
+        }
+    }
+
+    public void paintSquare(Graphics2D g2, Zetoni zeton, Koordinati koordinati) {
+        int x = koordinati.getX();
+        int y = koordinati.getY();
+        double w = squareWidth();
+        double i = x * w;
+        double j = y * w;
+
+        if (zeton.equals(Zetoni.BELI)) {
+            g2.setColor(Color.WHITE);
+        } else {
+            g2.setColor(Color.BLACK);
+        }
+
+        g2.setStroke(new BasicStroke((float) (w * LINE_WIDTH)));
+        g2.fillOval((int) i, (int) j, (int) w, (int) w);
     }
 
     @Override
-    public void mouseClicked(MouseEvent arg0) {
+    public void mousePressed(MouseEvent e) {
+        if (Vodja.kdoJeNaVrsti().equals(Vrsta.CLOVEK)) {
+            int x = e.getX();
+            int y = e.getY();
+            int w = (int) (squareWidth());
+            int i = x / w;
+            int j = y / w;
+            Koordinati k = new Koordinati(i, j);
+            Vodja.getIgra().odigraj(k);
+            Vodja.cikel();
+        }
 
     }
 
@@ -30,8 +111,16 @@ public class Platno extends JPanel implements MouseListener {
 
     }
 
+    public static double getLineWidth() {
+        return LINE_WIDTH;
+    }
+
+    public static double getPadding() {
+        return PADDING;
+    }
+
     @Override
-    public void mousePressed(MouseEvent arg0) {
+    public void mouseClicked(MouseEvent arg0) {
 
     }
 
