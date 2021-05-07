2c297f9307c7e1f01dc837a4ee3283aac6c22c64
David Čadež <david.cadez7@gmail.com>
2021-05-08 00:24:16 +0200

    Dodal razred, ki bo ocenjeval pozicije

---
+++ src/inteligenca/OceniPozicijo.java
@@ -0,0 +1,10 @@
+package inteligenca;
+
+import logika.Igra;
+import logika.Zetoni;
+
+public class OceniPozicijo {
+    public static int oceniPozicijo(Igra igra) {
+        Zetoni naPotezi = igra.getNapotezi();
+    }
+}
