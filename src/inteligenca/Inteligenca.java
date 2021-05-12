970687d396bc8743d7188c854855aa215e3982d2
David Čadež <david.cadez7@gmail.com>
2021-05-12 21:04:27 +0200

    Iz alphaBeta sem dal v Inteligenco

--- src/inteligenca/Inteligenca.java
+++ src/inteligenca/Inteligenca.java
@@ -1,18 +1,130 @@
 package inteligenca;
 
-import java.util.Map;
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Set;
 
 import logika.Igra;
 import logika.Zetoni;
 import splosno.KdoIgra;
 import splosno.Koordinati;
 
-public abstract class Inteligenca extends KdoIgra {
+public class Inteligenca extends KdoIgra {
 
-    public Inteligenca(String ime) {
-        super(ime);
+    private static final int ZMAGA = Integer.MAX_VALUE;
+    private static final int PORAZ = -ZMAGA;
+    private static final int NEODLOCENO = 0;
+    private int globina;
+
+    public Inteligenca(int g) {
+        super("AlphaBeta (" + g + ")");
+        globina = g;
     }
 
-    public abstract Map<Koordinati, Integer> izberiPotezo(Igra igra, int globina, Zetoni zetoni);
+    public Koordinati izberiPotezo(Igra igra) {
+        return alphaBeta(igra, Integer.MIN_VALUE, Integer.MAX_VALUE, igra.getNaPotezi()).getRandom();
+    }
+
+    public OcenjenePoteze alphaBeta(Igra igra, int alpha, int beta, Zetoni zetoni) {
+        Set<Koordinati> moznePoteze = igra.moznePoteze();
+
+        int ocena;
+        if (igra.getNaPotezi() == zetoni) {
+            ocena = PORAZ;
+        } else {
+            ocena = ZMAGA;
+        }
+
+        Koordinati kandidat = null;
+
+        for (Koordinati p : moznePoteze) {
+            Igra kopijaIgre = igra.clone();
+            kopijaIgre.odigraj(p);
+            int ocenap;
+            ocenap = (kopijaIgre.getNaPotezi() == zetoni ? PORAZ : ZMAGA);
+            switch (kopijaIgre.getStanje()) {
+                case ZMAGA_BELI:
+                    ocenap = (Zetoni.BELI == zetoni ? ZMAGA : PORAZ);
+                    break;
+                case ZMAGA_CRNI:
+                    ocenap = (Zetoni.CRNI == zetoni ? PORAZ : ZMAGA);
+                    break;
+                case NEODLOCENO:
+                    ocenap = NEODLOCENO;
+                    break;
+                case V_TEKU:
+                    if (globina == 1) {
+                        int x = kopijaIgre.getNaPotezi() == zetoni ? 1 : -1;
+                        ocenap = x * OceniPozicijo.oceniPozicijo(kopijaIgre);
+                    } else {
+                        Inteligenca nizje = new Inteligenca(globina - 1);
+                        ocenap = nizje.alphaBeta(kopijaIgre, alpha, beta, zetoni).getOcena();
+                    }
+            }
+            if (igra.getNaPotezi() == zetoni) {
+                if (ocenap > ocena) {
+                    ocena = ocenap;
+                    kandidat = p;
+                    alpha = Math.max(alpha, ocena);
+                }
+            } else {
+                if (ocenap < ocena) {
+                    ocena = ocenap;
+                    kandidat = p;
+                    beta = Math.min(beta, ocena);
+                }
+            }
+            if (alpha >= beta) {
+                return new OcenjenePoteze(ocena, kandidat);
+            }
+        }
+        return new OcenjenePoteze(ocena, kandidat);
+    }
+
+}
+
+class OcenjenePoteze {
+    private int ocena;
+    private Set<Koordinati> poteze;
+
+    public OcenjenePoteze(int o, Koordinati k) {
+        ocena = o;
+        poteze = new HashSet<Koordinati>();
+        addPoteza(k);
+    }
+
+    public void addPoteza(Koordinati k) {
+        poteze.add(k);
+    }
+
+    public int getOcena() {
+        return ocena;
+    }
+
+    public void setOcena(int ocena) {
+        this.ocena = ocena;
+    }
+
+    public Set<Koordinati> getPoteze() {
+        return poteze;
+    }
+
+    public void setPoteze(Set<Koordinati> poteze) {
+        this.poteze = poteze;
+    }
+
+    public Koordinati getRandom() {
+        int size = poteze.size();
+        int x = new Random().nextInt(size);
+        int i = 0;
+        for (Koordinati k : poteze) {
+            if (i == x) {
+                return k;
+            } else {
+                i++;
+            }
+        }
+        return getRandom();
+    }
 
 }
