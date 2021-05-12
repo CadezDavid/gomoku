3d75edecd7ed4df1a786c4f31b589860ec3b8937
David Čadež <david.cadez7@gmail.com>
2021-05-12 21:06:11 +0200

    Dodal funkcijo, ki oceni pozicijo

--- src/inteligenca/OceniPozicijo.java
+++ src/inteligenca/OceniPozicijo.java
@@ -1,10 +1,13 @@
 package inteligenca;
 
 import logika.Igra;
+import logika.Polje;
 import logika.Zetoni;
 
 public class OceniPozicijo {
     public static int oceniPozicijo(Igra igra) {
-        Zetoni naPotezi = igra.getNapotezi();
+        Zetoni naPotezi = igra.getNaPotezi();
+        Polje[][] plosca = igra.getPlosca();
+        return Groznje.groznje(plosca, naPotezi);
     }
 }
