ca5d3f3b62044f70dbdf2a730c14db6f6792c94a
MancaStrah <manca.strah@gmail.com>
2021-05-22 19:55:24 +0200

    Dodana/preurejena razreda OcenjenaPoteza in NajboljeOcenjenaPoteza ter nekaj sprememb

--- src/inteligenca/Inteligenca.java
+++ src/inteligenca/Inteligenca.java
@@ -1,6 +1,9 @@
 package inteligenca;
 
 import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Random;
 import java.util.Set;
 
 import logika.Igra;
@@ -20,11 +23,17 @@ public class Inteligenca extends KdoIgra {
         globina = g;
     }
 
+    // Vrne koordinati ene od potez z največjo vrednostjo. 
     public Koordinati izberiPotezo(Igra igra) {
         return alphaBeta(igra, Integer.MIN_VALUE, Integer.MAX_VALUE, igra.getNaPotezi()).getRandom();
     }
 
-    public OcenjenePoteze alphaBeta(Igra igra, int alpha, int beta, Zetoni zetoni) {
+    /**
+     * Algoritem alphaBeta, preveri in 
+     * oceni poteze in 
+     * vrne seznam (razred NajboljeOcenjenePoteze) najbolje ocenjenih potez
+     */
+    public NajboljeOcenjenePoteze alphaBeta(Igra igra, int alpha, int beta, Zetoni zetoni) {
         Set<Koordinati> moznePoteze = igra.moznePoteze();
 
         int ocena;
@@ -35,6 +44,7 @@ public class Inteligenca extends KdoIgra {
         }
 
         Koordinati kandidat = null;
+        NajboljeOcenjenePoteze najboljeOcenjenePoteze = new NajboljeOcenjenePoteze();
 
         for (Koordinati p : moznePoteze) {
             Igra kopijaIgre = igra.clone();
@@ -57,7 +67,7 @@ public class Inteligenca extends KdoIgra {
                         ocenap = x * OceniPozicijo.oceniPozicijo(kopijaIgre);
                     } else {
                         Inteligenca nizje = new Inteligenca(globina - 1);
-                        ocenap = nizje.alphaBeta(kopijaIgre, alpha, beta, zetoni).getOcena();
+                        ocenap = nizje.alphaBeta(kopijaIgre, alpha, beta, zetoni).getFirst().getOcena();
                     }
             }
             if (igra.getNaPotezi() == zetoni) {
@@ -74,60 +84,124 @@ public class Inteligenca extends KdoIgra {
                 }
             }
             if (alpha >= beta) {
-                return new OcenjenePoteze(ocena, kandidat);
+            	OcenjenaPoteza ocenjenaPoteza = new OcenjenaPoteza(ocena, kandidat);
+                najboljeOcenjenePoteze.addIfBest(ocenjenaPoteza);
             }
         }
-        return new OcenjenePoteze(ocena, kandidat);
-    }
+        OcenjenaPoteza ocenjenaPoteza = new OcenjenaPoteza(ocena, kandidat);
+        najboljeOcenjenePoteze.addIfBest(ocenjenaPoteza);
+        return najboljeOcenjenePoteze;
 
 }
 
-class OcenjenePoteze {
-    private int ocena;
-    private Set<Koordinati> poteze;
+class OcenjenaPoteza {
+	
+	 	private int ocena;
+	    private Koordinati poteza;
 
-    public OcenjenePoteze(int o, Koordinati k) {
-        ocena = o;
-        poteze = new HashSet<Koordinati>();
-        addPoteza(k);
-    }
+	    public OcenjenaPoteza(int o, Koordinati k) {
+	        ocena = o;
+	        poteza = k;
+	    }
 
-    public void addPoteza(Koordinati k) {
-        poteze.add(k);
-    }
 
-    public int getOcena() {
-        return ocena;
-    }
+	    public int getOcena() {
+	        return ocena;
+	    }
 
-    public void setOcena(int ocena) {
-        this.ocena = ocena;
-    }
+	    public void setOcena(int ocena) {
+	        this.ocena = ocena;
+	    }
 
-    public Set<Koordinati> getPoteze() {
-        return poteze;
-    }
+	    public Koordinati getPoteza() {
+	        return poteza;
+	    }
 
-    public void setPoteze(Set<Koordinati> poteze) {
-        this.poteze = poteze;
-    }
+	    public void setPoteza(Koordinati p) {
+	        this.poteza = p;
+	    }
 
-    public Koordinati getRandom() {
-        for (Koordinati k : poteze) {
-            return k;
-        }
-        // return getRandom();
-        // int size = poteze.size();
-        // int x = new Random().nextInt(size);
-        // int i = 0;
-        // for (Koordinati k : poteze) {
-        //     if (i == x) {
-        //         return k;
-        //     } else {
-        //         i++;
-        //     }
-        // }
-        return getRandom();
-    }
+//	Random random = new Random();
+//    private int ocena;
+//    private List<Koordinati> poteze;
+//
+//    public OcenjenePoteze() {
+//        ocena = Integer.MIN_VALUE;
+//        poteze = new LinkedList<Koordinati>();
+//    }
+//
+//    public void addPoteza(int o, Koordinati k) {
+//    	if (o > ocena) {
+//    		ocena = o;
+//    		poteze = new LinkedList<Koordinati>();
+//    		poteze.add(k);
+//    	} else if (o == ocena) {
+//    		poteze.add(k);
+//    	}
+//    }
+//
+//    public int getOcena() {
+//        return ocena;
+//    }
+//
+//    public void setOcena(int ocena) {
+//        this.ocena = ocena;
+//    }
+//
+//    public List<Koordinati> getPoteze() {
+//        return poteze;
+//    }
+//
+//    public void setPoteze(List<Koordinati> poteze) {
+//        this.poteze = poteze;
+//    }
+//
+//    public Koordinati getRandom() {
+//         int size = poteze.size();
+//         int x = new Random().nextInt(size + 1);
+//         return poteze.get(x);
+//    }
 
 }
+
+class NajboljeOcenjenePoteze {
+	private LinkedList<OcenjenaPoteza> buffer;
+	
+	public NajboljeOcenjenePoteze() {
+		this.buffer = new LinkedList<OcenjenaPoteza>();
+	}
+	
+	public void addIfBest(OcenjenaPoteza ocenjenaPoteza) {
+		if (buffer.isEmpty()) buffer.add(ocenjenaPoteza);
+		else {
+			OcenjenaPoteza op = buffer.getFirst();
+			int ocena = op.getOcena();
+			int o = ocenjenaPoteza.getOcena();
+			if (o > ocena) {
+	    		ocena = o;
+	    		buffer = new LinkedList<OcenjenaPoteza>();
+	    		buffer.add(ocenjenaPoteza);
+	    	} else if (o == ocena) {
+	    		buffer.add(ocenjenaPoteza);
+	    	}
+	    }
+	}
+	public OcenjenaPoteza getFirst() {
+		return buffer.get(0);
+	}
+	
+	public List<OcenjenaPoteza> list() {
+		return (List<OcenjenaPoteza>) buffer;
+	}
+	
+	public Koordinati getRandom() {
+		if (buffer.isEmpty()) return null;
+		   int size = buffer.size();
+           int x = new Random().nextInt(size);
+           OcenjenaPoteza p = buffer.get(x);
+           return p.getPoteza();
+	}
+}
+}
+
+	
