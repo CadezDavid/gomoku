901010abc3637e37cd8719adbec7c8e015f46730
MancaStrah <manca.strah@gmail.com>
2021-06-04 12:40:40 +0200

    Komentarji

--- src/inteligenca/OceniPozicijo.java
+++ src/inteligenca/OceniPozicijo.java
@@ -5,6 +5,12 @@ import logika.Zetoni;
 
 public class OceniPozicijo {
 	
+	/**
+	 * S številom oceni razporeditev plošče z vidika igralca naPotezi.
+	 * @param plosca - plošča, ki jo ocenjujemo
+	 * @param naPotezi - igralec na potezi
+	 * @return - številčna ocena
+	 */
     public static int oceniPozicijo(Polje[][] plosca, Zetoni naPotezi) {
         return Groznje.groznje(plosca, naPotezi);
     }
